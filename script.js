04021a621d304516f83bb83c8bcec4bcbc2b69f8
Dantesiio <DavidDo29@hotmail.com>
2024-12-11 13:41:40 -0500

    nueva solicitud

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("picnicBtn").addEventListener("click", function() {
+  const invitation = document.getElementById("invitation");
+  invitation.classList.toggle("hidden");
+});
